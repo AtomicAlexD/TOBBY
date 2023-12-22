5151b39d05630cf6421d47380b41d5b9c9ecd11c
Jake Pullen <jake_pullen@live.co.uk>
2023-12-22 13:46:10 +0000

    Add guild and message event handlers

--- bot.py
+++ bot.py
@@ -1,16 +1,57 @@
 import os
-
+import random 
 import discord
+
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = 'The Test Of Brewdog Boys Interactive'
 
 intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    print(f'message top level {message}')
+    print(f'author: {message.author}')
+    print(f'content: {message.content}')
+    if message.author == client.user:
+        return
+
+    print(message.content)
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif 'happy birthday' in message.content.lower():
+        await message.channel.send('Happy Birthday! 🎈🎉')
+
 
 client.run(TOKEN)
